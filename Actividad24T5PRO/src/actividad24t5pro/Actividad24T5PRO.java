b03f5b09855fa7e01e3005955b4b4df5cb366c8a
WhanAnAngel <AngelIlaiAlcaizGarc@AULA4-PC1>
2024-01-23 13:41:32 +0100

    Version3

---
+++ Actividad24T5PRO/src/actividad24t5pro/Actividad24T5PRO.java
@@ -0,0 +1,16 @@
+package actividad24t5pro;
+
+
+public class Actividad24T5PRO {
+
+    public static void main(String[] args) {
+    //Crea una clase empleado y una subclase encargado. Los encargados reciben un 
+    //10% más de sueldo base que un empleado normal aunque realicen el mismo trabajo. 
+    //Implementa dichas clases en el paquete objetos y sobrescribe el método getSueldo() para 
+    //ambas clases.
+    
+
+
+    }
+
+}
