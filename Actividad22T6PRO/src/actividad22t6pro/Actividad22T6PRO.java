fbea3f7cef643067ceb093833f2a76683199fa93
Angel Ilai Alcañiz García <anaga@campusaula.com>
2024-02-12 14:15:00 +0100

    :)

---
+++ Actividad22T6PRO/src/actividad22t6pro/Actividad22T6PRO.java
@@ -0,0 +1,10 @@
+package actividad22t6pro;
+
+
+public class Actividad22T6PRO {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Actividad26T6PRO/src/actividad26t6pro/Actividad26T6PRO.java
@@ -0,0 +1,10 @@
+package actividad26t6pro;
+
+
+public class Actividad26T6PRO {
+
+    public static void main(String[] args) {
+
+    }
+
+}
