ce6beca3e00d414933a7865fa9a07a297af2758c
angelilai <angelilai@192.168.1.38>
2024-03-11 21:31:01 +0100

    Commit automático

--- ActividadAdicionalT7/src/actividadadicionalt7/ActividadAdicionalT7.java
+++ ActividadAdicionalT7/src/actividadadicionalt7/ActividadAdicionalT7.java
@@ -4,36 +4,70 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-
+import java.util.Scanner;
 
 public class ActividadAdicionalT7 {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
-        //Nombre literal del fichero a leer
-        String file = "temperaturas.csv";
+        String lineaFichero;
         
+        int numLineas = 0;
+        // Nombre literal del fichero a leer
+        String file = "temperaturas.csv";
+
+        // Abres la movida del FileFeader y el BufferedReader (...)
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
-        String[] datosFichero = new String[file.length()*3];
-        String[] temperaturas = new String[file.length()];
-        String[] provincia = new String[file.length()];
-        String[] fecha = new String[file.length()];
-        String textoFichero;
-        int i = 0;
-        int j = 0;
 
+        // (...) y lo usas para saber el número de líneas
+        while ((lineaFichero = br.readLine()) != null) {
+            numLineas++;
+        }
+
+        // Creas el array de temperaturas del tamaño del num del líneas del csv
+        Temperatura[] temperaturas = new Temperatura[numLineas];
+
+        // Hay que cerrar el BufferedReader actual o le da un chungo y explota
+        br.close();
+
+        // Vuelves a abrir el archivo y el BufferedReader para leerlo
+        fr = new FileReader(file);
+        br = new BufferedReader(fr);
+
+        int i = 0; // Nunca sé dónde declarar los contadores así que lo meto aquí
         
-        while ((textoFichero=br.readLine()) != null){
-            datosFichero[i] = textoFichero;
+        /* Bucle que lee cada línea del fichero csv, crea un objeto tipo Temperatura
+        pasándole la línea como parámetro, y lo mete en el array de temperaturas
+        que de hecho se llama temperaturas */
+        while ((lineaFichero = br.readLine()) != null) {
+            // En cada vuelta sobreescribe el valor de "temp" por decirlo así
+            Temperatura temp = new Temperatura(lineaFichero);
+            temperaturas[i] = temp; /* Mete en la posición "i" del array el valor 
+            que tenga en esa iteración la variable "temp" */
             i++;
         }
+
+        // Pedimos una ubicación por teclado
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce una ubicación: ");
+        String ubicacionIntroducida = sc.nextLine();
         
-        String[] aaaa = datosFichero[0].split(",");
-        
-        System.out.println(Arrays.toString(aaaa));
-        fr.close();
+        /* Por cada objeto de tipo "Temperatura" (al cual llamaremos "tempr", por ejemplo, 
+        para referirnos a él dentro del bucle) que hay en el array 
+        de objetos de tipo "Temperatura" que se llama "temperaturas" (...) */
+        for (Temperatura tempr : temperaturas) {
+            
+            // (...) pilla el valor de su atributo "ubicacion" usando el getUbicacion
+            // y lo compara con el valor de la ubicación introducida por teclado
+            if ((tempr.getUbicacion().toLowerCase()) // Los pasa a minúsculas para evitar errores
+                    .equals(ubicacionIntroducida.toLowerCase())) {
+                
+                // Si coinciden, imprime el toString() de ese objeto Temperatura
+                System.out.println(tempr.toString());
+            }
+        }
+
     }
 
 }
